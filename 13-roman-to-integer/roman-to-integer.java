f6a9c5312ea98e7471c1092556eeae9d83ae59af
vatsal <nagarvatsal196@gmail.com>
2026-08-11 18:44:33 +0530

    Time: 3 ms (77.94%) | Memory: 47.5 MB (6.00%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Check two-character special cases
+            if (i < s.length() - 1) {
+
+                if (s.charAt(i) == 'I' && s.charAt(i + 1) == 'V') {
+                    sum += 4;
+                    i++;
+                    continue;
+                }
+
+                else if (s.charAt(i) == 'I' && s.charAt(i + 1) == 'X') {
+                    sum += 9;
+                    i++;
+                    continue;
+                }
+
+                else if (s.charAt(i) == 'X' && s.charAt(i + 1) == 'L') {
+                    sum += 40;
+                    i++;
+                    continue;
+                }
+
+                else if (s.charAt(i) == 'X' && s.charAt(i + 1) == 'C') {
+                    sum += 90;
+                    i++;
+                    continue;
+                }
+
+                else if (s.charAt(i) == 'C' && s.charAt(i + 1) == 'D') {
+                    sum += 400;
+                    i++;
+                    continue;
+                }
+
+                else if (s.charAt(i) == 'C' && s.charAt(i + 1) == 'M') {
+                    sum += 900;
+                    i++;
+                    continue;
+                }
+            }
+
+            // Normal single-character cases
+            if (s.charAt(i) == 'I') {
+                sum += 1;
+            }
+            else if (s.charAt(i) == 'V') {
+                sum += 5;
+            }
+            else if (s.charAt(i) == 'X') {
+                sum += 10;
+            }
+            else if (s.charAt(i) == 'L') {
+                sum += 50;
+            }
+            else if (s.charAt(i) == 'C') {
+                sum += 100;
+            }
+            else if (s.charAt(i) == 'D') {
+                sum += 500;
+            }
+            else if (s.charAt(i) == 'M') {
+                sum += 1000;
+            }
+        }
+
+        return sum;
+    }
+}
