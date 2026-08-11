60abe670929756c17671084ff11c7b1929c599c5
vatsal <nagarvatsal196@gmail.com>
2026-08-11 17:37:57 +0530

    Time: 1 ms (23.50%) | Memory: 43 MB (34.49%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count =0;
+        int max = 0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='(')count++;
+            max=Math.max(max,count);
+            if(s.charAt(i)==')')count--;
+            
+        }
+        return max;
+    }
+}
